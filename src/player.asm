3cec21c4a2dce437f5b2c01369dc66900a6b4bd2
rodrigo <rodrigo.garcia@estudiantes.matcom.uh.cu>
2018-11-17 13:55:58 +0000

    modified form of the ship, local vars, and method player.init at player.asm

--- src/player.asm
+++ src/player.asm
@@ -2,19 +2,37 @@
 %include "video.inc"
 %include "keyboard.inc"
 
-%define SHIP.COORDS 5
+extern print
+extern clear
+extern erase
+extern scan
+
+%define SHIP.COORDS 6
+
+%macro SHIP.ROW 1
+    xor eax, eax
+    mov ax, [row.offset]
+    add ax, %1
+%endmacro
+
+%macro SHIP.COL 1
+    xor eax, eax
+    mov ax, [col.offset]
+    add ax, %1
+%endmacro
 
 ; Data section is meant to hold constant values, do not modify
 section .data
 
-graphics dw '/'|FG.YELLOW|BG.BLACK,\
-            '-'|FG.YELLOW|BG.BLACK,\
+graphics dw 'A'|FG.YELLOW|BG.BLACK,\
+            '('|FG.YELLOW|BG.BLACK,\
+            ')'|FG.YELLOW|BG.BLACK,\
             '^'|FG.YELLOW|BG.BLACK,\
-            '-'|FG.YELLOW|BG.BLACK,\
-            '\'|FG.YELLOW|BG.BLACK
+            'W'|FG.YELLOW|BG.BLACK,\
+            'W'|FG.YELLOW|BG.BLACK,
 
-rows dw 0, 0, 0, 0, 0
-cols dw 0, 1, 2, 3, 4
+rows dw 0, 1, 1, 3, 3, 3
+cols dw 2, 1, 3, 2, 1, 3
 
 row.top dw 0
 row.bottom dw 0
@@ -24,18 +42,42 @@ col.right dw 4
 
 section .bss
 
+map resd 1
+canvas resd 1
+
 lives resw 1
 
 row.offset resw 1
 col.offset resw 1
 
+hash resw 1
+
 section .text
 
-; init(dword lives, dword r.offset, dword c.offset)
+; init(word lives, word r.offset, word c.offset, word hash, dword map, dword canvas)
 ; Initialize player
 global player.init
 player.init:
     FUNC.START
+    ;filling local vars of player
+    mov bx, [PARAM.DW(0)]
+    mov [lives], bx
+    
+    mov bx, [PARAM.DW(1)]
+    mov [row.offset], bx
+    
+    mov bx, [PARAM.DW(2)]
+    mov [col.offset], bx
+    
+    mov bx, [PARAM.DW(3)]
+    mov [hash], bx
+    
+    mov bx, [PARAM.DW(4)]
+    mov [map], bx
+    
+    mov bx, [PARAM.DW(5)]
+    mov [canvas], bx
+    
     FUNC.END
 
 ; update(dword *map)
