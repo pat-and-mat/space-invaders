569569df6622b1501133c3a5d82f9353360fea27
Jorge Mederos <jorge.mederos@estudiantes.matcom.uh.cu>
2018-11-18 23:59:10 -0500

    Set up 'game.asm' with basic game loop

--- src/game.asm
+++ src/game.asm
@@ -1,5 +1,11 @@
 %include "video.inc"
 %include "keyboard.inc"
+%include "stack.inc"
+
+global input
+section .bss
+
+input resb 1
 
 section .text
 
@@ -7,26 +13,13 @@ extern video.clear
 extern scan
 extern calibrate
 
-; Bind a key to a procedure
-%macro bind 2
-  cmp byte [esp], %1
-  jne %%next
-  call %2
-  %%next:
-%endmacro
-
-; Fill the screen with the given background color
-%macro FILL_SCREEN 1
-  push word %1
-  call video.clear
-  add esp, 2
-%endmacro
+extern engine.run
 
 global game
 game:
   ; Initialize game
 
-  FILL_SCREEN BG.BLACK
+  CALL video.clear, BG.BLACK
 
   ; Calibrate the timing
   call calibrate
@@ -34,35 +27,11 @@ game:
   ; Game main loop
   game.loop:
     .input:
-      call get_input
+      call scan
+      mov [input], al
 
     ; Main loop.
 
-    ; Here is where you should place your game's logic.
-    ; Develop procedures like paint_map and update_content,
-    ; declare them extern and use them here.
+    engine.run
 
     jmp game.loop
-
-
-draw.red:
-  FILL_SCREEN BG.RED
-  ret
-
-
-draw.green:
-  FILL_SCREEN BG.GREEN
-  ret
-
-
-get_input:
-    call scan
-    push ax
-    ; The value of the input is on 'word [esp]'
-
-    ; Your bindings here
-    bind KEY.UP, draw.green
-    bind KEY.DOWN, draw.red
-
-    add esp, 2 ; free the stack
-    ret
